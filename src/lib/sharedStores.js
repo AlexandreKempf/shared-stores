f2285e36585294934679bc7bf873f15560235e07
Aleexo <alexandre@lixo.tech>
2022-11-11 15:13:59 +0100

    working example

--- src/lib/sharedStores.js
+++ src/lib/sharedStores.js
@@ -2,16 +2,25 @@ import { writable } from 'svelte/store';
 import { supabase } from '$lib/supabase';
 
 
-export async function sharedStore(store_name) {
+export async function sharedStore(store_name, init_value = null) {
 
-    const store = writable();
+    const store = writable(init_value);
 
     const channel = supabase.channel(store_name)
 
-    // listen to broadcasts
+    let store_value = init_value
+
     channel
-        .on('broadcast', { event: 'store-update' }, ({ event, payload, type }) => store.set(payload))
+        .on('broadcast', { event: 'store-update' }, ({ event, payload, type }) => { payload === init_value ? null : set_locally(payload) })
+        .on('presence', { event: 'join' }, ({ newuser }) => { store_value === init_value ? null : set(store_value) })
         .subscribe()
+        .track({ online_at: new Date().toISOString() })
+
+
+    function set_locally(value) {
+        store.set(value)
+        store_value = value
+    }
 
     async function set(value) {
         await channel.send({
@@ -19,12 +28,13 @@ export async function sharedStore(store_name) {
             event: 'store-update',
             payload: value
         })
-        store.set(value)
+        set_locally(value)
     }
+
+
 
     return {
         subscribe: store.subscribe,
         set: set,
     };
 }
-
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <script>
 	import { my_store } from '../lib/myStore';
-	$my_store = 1;
 </script>
-
-<!-- https://supabase.com/docs/guides/realtime/presence -->
-<!-- https://supabase.com/blog/supabase-realtime-multiplayer-general-availability -->
 
 <button
 	on:click={() => {
 		$my_store += 1;
 	}}
 >
-	random int
+	Accumulator
 </button>
 
 <p>{$my_store}</p>
